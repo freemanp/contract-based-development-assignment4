805766668c931490bb0cb57d2a50cd6c348a321c
Andrius <Andrius.or@gmail.com>
2013-04-16 17:05:15 +0200

    Defined the full interface api thing.

--- src/main/java/dk/cphbusiness/appAPI/App.java
+++ src/main/java/dk/cphbusiness/appAPI/App.java
@@ -1,5 +1,6 @@
 package dk.cphbusiness.appAPI;
 
+import dk.cphbusiness.appAPI.dtos.EvaluationDTO;
 import dk.cphbusiness.appAPI.dtos.ItemDTO;
 import dk.cphbusiness.appAPI.dtos.MemberDTO;
 import dk.cphbusiness.appAPI.etos.IllegalStateETO;
@@ -9,6 +10,56 @@ public class App implements IAppAPI {
 
 	public void lendItem(MemberDTO fromMember, MemberDTO toMember, ItemDTO item)
 			throws NullPointerETO, IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void createMember(MemberDTO newMember) throws NullPointerETO,
+			IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void createItem(ItemDTO item) throws NullPointerETO, IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void searchItems(String searchTerm) throws NullPointerETO,
+			IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void evaluateItem(EvaluationDTO evalution, ItemDTO item,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void evaluateLender(EvaluationDTO evaluation, MemberDTO evaluatee,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void evaluateBorrower(EvaluationDTO evaluation, MemberDTO evaluatee,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void evaluateHandBack(EvaluationDTO evaluation, ItemDTO item,
+			MemberDTO evaluatee, MemberDTO evaluator) throws NullPointerETO,
+			IllegalStateETO {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void checkMemberProfile(MemberDTO member) throws NullPointerETO,
+			IllegalStateETO {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/dk/cphbusiness/appAPI/IAppAPI.java
+++ src/main/java/dk/cphbusiness/appAPI/IAppAPI.java
@@ -1,5 +1,6 @@
 package dk.cphbusiness.appAPI;
 
+import dk.cphbusiness.appAPI.dtos.EvaluationDTO;
 import dk.cphbusiness.appAPI.dtos.ItemDTO;
 import dk.cphbusiness.appAPI.dtos.MemberDTO;
 import dk.cphbusiness.appAPI.etos.IllegalStateETO;
@@ -8,4 +9,28 @@ import dk.cphbusiness.appAPI.etos.NullPointerETO;
 public interface IAppAPI {
 	public void lendItem(MemberDTO fromMember, MemberDTO toMember, ItemDTO item)
 			throws NullPointerETO, IllegalStateETO;
+
+	public void createMember(MemberDTO newMember) throws NullPointerETO,
+			IllegalStateETO;
+
+	public void createItem(ItemDTO item) throws NullPointerETO, IllegalStateETO;
+
+	public void searchItems(String searchTerm) throws NullPointerETO,
+			IllegalStateETO;
+
+	public void evaluateItem(EvaluationDTO evalution, ItemDTO item,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO;
+
+	public void evaluateLender(EvaluationDTO evaluation, MemberDTO evaluatee,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO;
+
+	public void evaluateBorrower(EvaluationDTO evaluation, MemberDTO evaluatee,
+			MemberDTO evaluator) throws NullPointerETO, IllegalStateETO;
+
+	public void evaluateHandBack(EvaluationDTO evaluation, ItemDTO item,
+			MemberDTO evaluatee, MemberDTO evaluator) throws NullPointerETO,
+			IllegalStateETO;
+
+	public void checkMemberProfile(MemberDTO member) throws NullPointerETO,
+			IllegalStateETO;
 }
